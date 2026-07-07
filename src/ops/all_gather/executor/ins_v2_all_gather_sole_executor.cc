719cc6ca13b4c0cc334ea5495052ae4dcf74666c
HafenW <wanglizhi19@huawei.com>
2026-07-07 14:43:33 +0800

    对称内存相关偏移量设置只在InsAllGatherMesh1D算法生效
    Co-authored-by: HafenW<wanglizhi19@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1785 merge cherry-pick-mr-1784-1783305157546-auto into 9.1.0
    
    对称内存相关偏移量设置只在InsAllGatherMesh1D算法生效
    
    Created-by: HafenW
    Commit-by: HafenW
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    对称内存相关偏移量设置只在InsAllGatherMesh1D算法生效
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR不涉及Issue，可填写"NA"。-->
    NA
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于构造对应xx测试用例、二级冒烟、算子泛化等。-->
    自测试用例 rdv 
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    NA
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    See merge request: cann/hccl!1785

--- src/ops/all_gather/executor/ins_v2_all_gather_sole_executor.cc
+++ src/ops/all_gather/executor/ins_v2_all_gather_sole_executor.cc
@@ -145,8 +145,8 @@ HcclResult InsV2AllGatherSoleExecutor<AlgTopoMatch, InsAlgTemplate>::Orchestrate
         maxDataSizePerLoop = transportBoundDataSize;
     }
 
-    // 如果是对称内存，每次传输的大小不受cclbuffer和UB_MAX_DATA_SIZE的限制
-    if (param.supportSymmetricMemory) {
+    // 如果是对称内存，且算法是InsAllGatherMesh1D，每次传输的大小不受cclbuffer和UB_MAX_DATA_SIZE的限制
+    if (param.supportSymmetricMemory && std::string(param.algName) == "InsAllGatherMesh1D") {
         maxDataSizePerLoop = dataSize_;
         tempAlgParams.supportSymmetricMemory = true;
     }
